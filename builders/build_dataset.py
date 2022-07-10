3b0572e1885ffaf9976ce520771f24918d8439e3
10cho <myeddie@naver.com>
2022-07-10 14:25:38 +0900

    pass additional arguments to transforms

--- builders/build_dataset.py
+++ builders/build_dataset.py
@@ -1,17 +1,16 @@
-import build_transforms
+from builders import build_transforms
 
 from data.datasets.image_classification import ImageClassificationDataset
 
 
 def build(config, task):
     if task == "classification":
-        input_transform = build_transforms.build(config.transforms.input)
-        gt_transform = build_transforms.build(config.transforms.gt)
-
-        dataset = ImageClassificationDataset(
-            root=config.dataset.root,
-            tf_input=input_transform,
-            tf_gt=gt_transform
+        input_transform = build_transforms.build(
+            config.transforms.input,
+            **dict(config.additional_params)
         )
 
-    return dataset
+        return ImageClassificationDataset(
+            root=config.root,
+            tf_input=input_transform
+        )
--- builders/build_transforms.py
+++ builders/build_transforms.py
@@ -1,10 +1,10 @@
 from data.transforms import pre_defined_transforms as tf
 
 
-def build(tf_type):
+def build(tf_type, **additional_params):
     if tf_type == "simple_tf":
-        transform = tf.simple_tf()
+        return tf.simple_tf()
     elif tf_type == "simple_tf_rescale":
-        transform = tf.simple_tf_rescale()
-
-    return transform
+        return tf.simple_tf_rescale()
+    elif tf_type == "standard_tf":
+        return tf.standard_tf(**additional_params)
