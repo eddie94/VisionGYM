17ba24d3e9ff7835b78715e6abaafbda7eb2e1c7
10cho <myeddie@naver.com>
2022-07-10 14:12:34 +0900

    standard image transform added

--- data/transforms/pre_defined_transforms.py
+++ data/transforms/pre_defined_transforms.py
@@ -4,12 +4,26 @@ from torchvision.transforms import Compose
 
 
 def simple_tf():
-    return Compose(
+    return Compose([
         tf.PILToTensor()
-    )
+    ])
 
 
 def simple_tf_rescale():
-    return Compose(
+    return Compose([
         tf.ToTensor()
-    )
+    ])
+
+
+def standard_tf(
+        img_size=64,
+        mean=(0.5, 0.5, 0.5),
+        std=(0.5, 0.5, 0.5),
+        p=0.5
+):
+    return Compose([
+        tf.ToTensor(),
+        tf.Resize(img_size),
+        tf.RandomHorizontalFlip(p=p),
+        tf.Normalize(mean=mean, std=std),
+    ])
