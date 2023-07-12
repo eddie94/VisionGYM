2d2db00f7f9e3fcb02b26de2857e35a32ba2b9cd
“Ueddie94 <myeddie@naver.com>
2023-07-12 12:45:17 +0900

    conv parameter assertion added

--- visiongym/common/param_config.py
+++ visiongym/common/param_config.py
@@ -1,10 +1,11 @@
 from typing import Any
 from collections.abc import Iterable
+from omegaconf import OmegaConf
 
 
 class Params:
-    def __init__(self) -> None:
-        self.params = {}
+    def __init__(self, param_dict: dict = {}) -> None:
+        self.params = param_dict
     
     def add_param(
         self,
@@ -23,7 +24,27 @@ class Params:
                 param_dict[key] = self.params[key]
         
         return param_dict
+    
+
+def assert_param_len(*params) -> bool:
+    """returns if all the parameters have the same length
+
+    Returns:
+        true if all parameters have the same length, false if not
+    """
+    it = iter(params)
+    param_len = len(next(it))
+    if not all(len(l) == param_len for l in it):
+        return False
+    else:
+        return True
 
 
-def assert_conv2d_options(config):
-    pass
+def assert_conv2d_options(config: OmegaConf) -> Params:
+    if "additional_params" in config:
+        param_dict = config.additional_params
+        assert assert_param_len(param_dict.values()), "parameters don't hane the same length"
+    else:
+        param_dict = {}
+    
+    return Params(param_dict)
